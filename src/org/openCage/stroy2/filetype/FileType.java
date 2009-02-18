64aca4fa137669140d49ad1ded8102ce9f031d32
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-02-18 16:01:00 +0000

    file util
    git-svn-id: https://stroy.googlecode.com/svn/trunk@121 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- src/org/openCage/stroy2/filetype/FileType.java
+++ src/org/openCage/stroy2/filetype/FileType.java
@@ -1,5 +1,9 @@
 package org.openCage.stroy2.filetype;
 
+import eu.medsea.util.MimeUtil;
+
+import java.io.File;
+
 /**
  * Created by IntelliJ IDEA.
  * User: spfab
@@ -8,4 +12,36 @@ package org.openCage.stroy2.filetype;
  * To change this template use File | Settings | File Templates.
  */
 public class FileType {
+
+    public static void main(String[] args) {
+
+        if ( args.length == 0 ) {
+            System.err.println("Usage: file <file>");
+            System.err.println("       Determines the type of the file");
+            System.err.println("       e.g. file duda.txt");
+            System.err.println("            text/plain");
+            System.exit(1);
+        }
+
+        System.out.println( MimeUtil.getExtensionMimeTypes( args[0]));
+        System.out.println( MimeUtil.getMagicMimeType( args[0]));
+
+
+//        System.out.println( MimeUtil.getExtensionMimeTypes( new File("Sys.jar")));
+//        System.out.println( MimeUtil.getExtensionMimeTypes( new File("C:\\prs\\stroy\\external\\production\\Sys.jar")));
+//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\external\\production\\Sys.jar")));
+//
+//        System.out.println( MimeUtil.getMagicMimeType( new File("Readme")));
+//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\home\\download\\MimeUtil\\Readme.txt")));
+//
+//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\native\\messages_ja.properties")));
+//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\native\\test_ja.properties")));
+//
+//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\Documents and Settings\\spfab\\Desktop\\Apress.Google.Guice.Agile.Lightweight.Dependency.Injection.Framework.Apr.2008.pdf")));
+//
+//        System.out.println( MimeUtil.getExtensionMimeTypes( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\Message.java")));
+//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\Message.java")));
+
+
+    }
 }
