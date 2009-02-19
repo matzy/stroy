fdbeb9e3afb29efebb3b55db4cad946779251083
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-02-19 19:00:53 +0000

    version to 1.0 file clt
    git-svn-id: https://stroy.googlecode.com/svn/trunk@122 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- src/org/openCage/stroy2/filetype/FileType.java
+++ src/org/openCage/stroy2/filetype/FileType.java
@@ -3,45 +3,89 @@ package org.openCage.stroy2.filetype;
 import eu.medsea.util.MimeUtil;
 
 import java.io.File;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
-/**
- * Created by IntelliJ IDEA.
- * User: spfab
- * Date: Feb 10, 2009
- * Time: 4:15:36 PM
- * To change this template use File | Settings | File Templates.
- */
+
+/***** BEGIN LICENSE BLOCK *****
+* Version: MPL 1.1
+*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Original Code is stroy code.
+*
+* The Initial Developer of the Original Code is Stephan Pfab <openCage@gmail.com>.
+* Portions created by Stephan Pfab are Copyright (C) 2006 - 2009.
+* All Rights Reserved.
+*
+* Contributor(s):
+***** END LICENSE BLOCK *****/
+
 public class FileType {
 
     public static void main(String[] args) {
 
-        if ( args.length == 0 ) {
-            System.err.println("Usage: file <file>");
-            System.err.println("       Determines the type of the file");
-            System.err.println("       e.g. file duda.txt");
-            System.err.println("            text/plain");
-            System.exit(1);
+       CLTArgs cltArgs = new CLTArgs( args );
+
+        if ( !cltArgs.isOk() ) {
+            return;
         }
 
-        System.out.println( MimeUtil.getExtensionMimeTypes( args[0]));
-        System.out.println( MimeUtil.getMagicMimeType( args[0]));
+        swallowWarningMessage();
+        
+        check( cltArgs, cltArgs.getFile(), "" );
 
 
-//        System.out.println( MimeUtil.getExtensionMimeTypes( new File("Sys.jar")));
-//        System.out.println( MimeUtil.getExtensionMimeTypes( new File("C:\\prs\\stroy\\external\\production\\Sys.jar")));
-//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\external\\production\\Sys.jar")));
-//
-//        System.out.println( MimeUtil.getMagicMimeType( new File("Readme")));
-//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\home\\download\\MimeUtil\\Readme.txt")));
-//
-//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\native\\messages_ja.properties")));
-//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\native\\test_ja.properties")));
-//
-//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\Documents and Settings\\spfab\\Desktop\\Apress.Google.Guice.Agile.Lightweight.Dependency.Injection.Framework.Apr.2008.pdf")));
-//
-//        System.out.println( MimeUtil.getExtensionMimeTypes( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\Message.java")));
-//        System.out.println( MimeUtil.getMagicMimeType( new File("C:\\prs\\stroy\\src\\org\\openCage\\stroy\\locale\\Message.java")));
+    }
 
+    private static void check(CLTArgs cltArgs, String file, String prefix ) {
 
+        checkFile(cltArgs, file, prefix  );
+
+        if ( cltArgs.isRecursive() && new File(file).isDirectory()) {
+            for ( File kid : new File( file ).listFiles()) {
+                check( cltArgs, kid.getAbsolutePath(), prefix + "  " );
+            }
+        }
+    }
+
+    private static void checkFile( CLTArgs cltArgs, String file, String prefix ) {
+
+        System.out.println( prefix + "Checking: " + file );
+
+        if ( !cltArgs.contentOnly() ) {
+            if ( cltArgs.isVerbose() ) {
+                System.out.print( prefix + "  By extension: ");
+            }
+            System.out.println( MimeUtil.getExtensionMimeTypes( file));
+        }
+
+        if ( !cltArgs.extensionOnly() ) {
+
+            if ( !new File(file).exists() ) {
+                System.err.println( prefix + "  file does not exist"  );
+                return;
+            }
+
+            if ( cltArgs.isVerbose() ) {
+                System.out.print( prefix + "  By content:   ");
+            }
+            System.out.println( MimeUtil.getMagicMimeType( file));
+        }
+    }
+
+    private static void swallowWarningMessage() {
+        // swallow the warning message that this is not UNIX, e.g. no magic at /etc/somesuch
+        Logger.getLogger("").setLevel( Level.SEVERE );
+        MimeUtil.getExtensionMimeTypes( "foo.txt");
+        Logger.getLogger("").setLevel( Level.INFO );
     }
 }
