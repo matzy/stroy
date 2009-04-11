aaf24cb0289cd87e69f0cb7dc8060152881bb1eb
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-04-11 17:11:20 +0000

    1.0.3 update
    git-svn-id: https://stroy.googlecode.com/svn/branches/stroy-1.0@147 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- src/org/openCage/stroy/app/StroyAppInfo.java
+++ src/org/openCage/stroy/app/StroyAppInfo.java
@@ -35,7 +35,7 @@ import java.util.List;
 @SuppressWarnings({"HardCodedStringLiteral"})
 public class StroyAppInfo implements AppInfo {
 
-    private final Version2        version         = new Version2( 1, 0, 2, 134 );
+    private final Version2        version         = new Version2( 1, 0, 3, 147 );
     private final List<Reference> refs            = new ArrayList<Reference>();
     private final List<Reference> buildRefs       = new ArrayList<Reference>();
     private final List<Change>    changes         = new ArrayList<Change>();
