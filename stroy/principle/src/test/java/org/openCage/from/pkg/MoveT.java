f7e3a76d24abe5ee18663e01931fb1ea42ea4b1c
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-06-28 09:24:15 +0000

    test move dir: move
    git-svn-id: https://stroy.googlecode.com/svn/trunk@151 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ stroy/principle/src/test/java/org/openCage/from/pkg/MoveT.java
@@ -0,0 +1,11 @@
+package org.openCage.from.pkg;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stephan
+ * Date: Jun 28, 2009
+ * Time: 11:18:12 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MoveT {
+}
