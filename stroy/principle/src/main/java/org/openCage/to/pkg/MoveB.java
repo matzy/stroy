6f68ac3f637c079eac0551ef23cb81dc70cac70e
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-06-28 09:16:28 +0000

    test move dir: move
    git-svn-id: https://stroy.googlecode.com/svn/trunk@150 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- stroy/principle/src/main/java/org/openCage/to/pkg/MoveB.java
+++ stroy/principle/src/main/java/org/openCage/to/pkg/MoveB.java
@@ -1,11 +1,11 @@
-package org.openCage.from.pkg;
+package org.openCage.to.pkg;
 
 /**
  * Created by IntelliJ IDEA.
  * User: stephan
  * Date: Jun 28, 2009
- * Time: 11:08:41 AM
+ * Time: 11:09:05 AM
  * To change this template use File | Settings | File Templates.
  */
-public class MoveA {
+public class MoveB {
 }
