1fcaa0af99ecd61d0e3bdf734e045a09b92aa71c
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-09-29 06:01:57 +0000

    comment
    git-svn-id: https://stroy.googlecode.com/svn/trunk@206 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/lang/src/org/openCage/lang/protocol/Builder.java
+++ modules/lang/src/org/openCage/lang/protocol/Builder.java
@@ -1,5 +1,12 @@
 package org.openCage.lang.protocol;
 
+/**
+ * Builder Pattern
+ * add data to a builder and at last call build to create a new object
+ * this replaces a complex constructor + init process
+ * @author stephan
+ * @param <T>
+ */
 public interface Builder<T> {
 	public T build();
 }
