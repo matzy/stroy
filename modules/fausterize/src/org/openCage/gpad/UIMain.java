40cccc864954cede14d3ebe09bd99a0c249a5886
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2009-11-30 16:30:30 +0000

    licence and tree fixes
    git-svn-id: https://stroy.googlecode.com/svn/trunk@293 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/fausterize/src/org/openCage/gpad/UIMain.java
+++ modules/fausterize/src/org/openCage/gpad/UIMain.java
@@ -3,13 +3,28 @@ package org.openCage.gpad;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-/**
- * Created by IntelliJ IDEA.
- * User: stephan
- * Date: Nov 1, 2009
- * Time: 8:14:52 AM
- * To change this template use File | Settings | File Templates.
- */
+/***** BEGIN LICENSE BLOCK *****
+* Version: MPL 1.1
+*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Original Code is stroy code.
+*
+* The Initial Developer of the Original Code is Stephan Pfab <openCage@gmail.com>.
+* Portions created by Stephan Pfab are Copyright (C) 2006 - 2009.
+* All Rights Reserved.
+*
+* Contributor(s):
+***** END LICENSE BLOCK *****/
+
 public class UIMain {
 
 
