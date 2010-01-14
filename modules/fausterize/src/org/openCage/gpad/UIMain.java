391eeabc246028eeca166b4ddaf2e9b619093337
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-01-14 08:00:22 +0000

    fix broken osx.app zipping
    git-svn-id: https://stroy.googlecode.com/svn/trunk@349 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/fausterize/src/org/openCage/gpad/UIMain.java
+++ modules/fausterize/src/org/openCage/gpad/UIMain.java
@@ -2,36 +2,56 @@ package org.openCage.gpad;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 
-/***** BEGIN LICENSE BLOCK *****
-* Version: MPL 1.1
-*
-* The contents of this file are subject to the Mozilla Public License Version
-* 1.1 (the "License"); you may not use this file except in compliance with
-* the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS" basis,
-* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-* for the specific language governing rights and limitations under the
-* License.
-*
-* The Original Code is stroy code.
-*
-* The Initial Developer of the Original Code is Stephan Pfab <openCage@gmail.com>.
-* Portions created by Stephan Pfab are Copyright (C) 2006 - 2010.
-* All Rights Reserved.
-*
-* Contributor(s):
-***** END LICENSE BLOCK *****/
+/**
+ * ** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * <p/>
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * <p/>
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * <p/>
+ * The Original Code is stroy code.
+ * <p/>
+ * The Initial Developer of the Original Code is Stephan Pfab <openCage@gmail.com>.
+ * Portions created by Stephan Pfab are Copyright (C) 2006 - 2010.
+ * All Rights Reserved.
+ * <p/>
+ * Contributor(s):
+ * **** END LICENSE BLOCK ****
+ */
 
 public class UIMain {
 
 
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector( new FausterizeWiring() );
+        Injector injector = Guice.createInjector(new FausterizeWiring());
 
-        FaustUI faust = injector.getInstance( FaustUI.class );
-        faust.setVisible(true);
+        CliOptions bean = new CliOptions();
+        CmdLineParser parser = new CmdLineParser(bean);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("java -jar myprogram.jar [options...] arguments...");
+            parser.printUsage(System.err);
+            return;
+        }
+
+        if ( bean.getFilepath() == null ) {
+            FaustUI faust = injector.getInstance(FaustUI.class);
+            faust.setVisible(true);
+        }
+
+        System.out.println("++++ " + bean.getFilepath());
+
     }
 }
