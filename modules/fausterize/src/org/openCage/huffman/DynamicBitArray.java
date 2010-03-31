7e2ebc1051bbc3ba332a58a663ad36801f1a267b
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-03-31 14:04:48 +0000

    oops 2
    git-svn-id: https://stroy.googlecode.com/svn/trunk@440 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/fausterize/src/org/openCage/huffman/DynamicBitArray.java
+++ modules/fausterize/src/org/openCage/huffman/DynamicBitArray.java
@@ -1,220 +1,34 @@
 package org.openCage.huffman;
 
-import org.openCage.lang.clazz.Count;
+/**
+ * Created by IntelliJ IDEA.
+ * User: stephan
+ * Date: Mar 12, 2010
+ * Time: 3:23:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DynamicBitArray {
+    DynamicBitArray append( boolean bit );
 
-import java.util.ArrayList;
-import java.util.List;
+    byte[] toByteArray();
 
-/***** BEGIN LICENSE BLOCK *****
-* Version: MPL 1.1
-*
-* The contents of this file are subject to the Mozilla Public License Version
-* 1.1 (the "License"); you may not use this file except in compliance with
-* the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS" basis,
-* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-* for the specific language governing rights and limitations under the
-* License.
-*
-* The Original Code is stroy code.
-*
-* The Initial Developer of the Original Code is Stephan Pfab <openCage@gmail.com>.
-* Portions created by Stephan Pfab are Copyright (C) 2006 - 2010.
-* All Rights Reserved.
-*
-* Contributor(s):
-***** END LICENSE BLOCK *****/
-public class DynamicBitArray {
+    String toString();
 
-    private List<Byte> bytes = new ArrayList<Byte>();
-    private int last = 0;
+    String toString8();
 
-    public DynamicBitArray() {
-        bytes.add( (byte)0);
-    }
+    DynamicBitArray append( DynamicBitArray other );
 
+    DynamicBitArray clone();
 
-    public DynamicBitArray append( boolean bit ) {
-        if ( bit ) {
-            byte by = bytes.get(bytes.size() -1);
-            by |= (byte) (1 << last);
-            bytes.set( bytes.size() -1, by );
-        }
+    Byte getByteModulo( int idx );
 
-        last++;
+    int getBitSize();
 
-        if ( last == 8 ) {
-            bytes.add( (byte)0 );
-            last = 0;
-        }
+    int toInt( int size );
 
-        return this;
-    }
+    int toInt( int from, int size );
 
-    public byte[] toByteArray() {
-        byte[] arr = new byte[ bytes.size() ];
-        int  i = 0;
-        for ( Byte by : bytes ) {
-            arr[i] = by;
-            i++;
-        }
+    DynamicBitArray getSlice( int from, int size );
 
-        return arr;
-    }
-
-    public String toString() {
-        String ret = "";
-        for ( Count<Byte> by : Count.count(bytes) ) {
-            int to = 8;
-            if ( by.isLast() ) {
-                to = last;
-            }
-
-            for ( int i = 0; i < to; i++ ) {
-                byte pattern = (byte) (1 << i);
-                if ( (by.obj() & pattern) == pattern ) {
-                    ret += "1";
-                } else {
-                    ret += "0";
-                }
-            }
-        }
-
-        if ( ret.equals("")) {
-            return "0";
-        }
-
-        return ret;
-    }
-
-    public String toString8() {
-        String ret = "DBA: ";
-        for ( Count<Byte> by : Count.count(bytes) ) {
-            int to = 8;
-            if ( by.isLast() ) {
-                to = last;
-            }
-
-            for ( int i = 0; i < to; i++ ) {
-                byte pattern = (byte) (1 << i);
-                if ( (by.obj() & pattern) == pattern ) {
-                    ret += "1";
-                } else {
-                    ret += "0";
-                }
-            }
-            ret += "|";
-        }
-
-        return ret;
-    }
-
-    public DynamicBitArray append( DynamicBitArray other ) {
-
-        String todo = other.toString();
-        for ( int i = 0; i < todo.length(); ++i ) {
-            append( todo.charAt(i) == '1' );
-        }
-        
-        return this;
-    }
-
-    public DynamicBitArray clone() {
-        DynamicBitArray dba = new DynamicBitArray();
-        dba.bytes = new ArrayList<Byte>( bytes );
-        dba.last = last;
-
-        return dba;
-    }
-
-    public Byte getByteModulo( int idx ) {
-        return bytes.get( idx % bytes.size() );
-    }
-
-    public int getBitSize() {
-        return ((bytes.size() - 1) * 8) + last;
-    }
-
-    public static DynamicBitArray toDba( int num ) {
-        if ( num < 0 ) {
-            throw new IllegalArgumentException();
-        }
-
-        DynamicBitArray ret = new DynamicBitArray();
-
-        int max = 0;
-        int val = 1;
-        while ( val <= num ) {
-            val *= 2;
-            max++;
-        }
-
-        for ( int i = 0; i < max; ++i ) {
-            byte pattern = (byte) (1 << i);
-            ret.append( (num & pattern) == pattern );                
-        }
-
-        return ret;
-    }
-
-    public static DynamicBitArray toDba( int i, int size ) {
-        DynamicBitArray ret = toDba( i );
-
-        while ( ret.getBitSize() < size ) {
-            ret.append( false );
-        }
-
-        return ret;
-    }
-
-    public int toInt( int size ) {
-        int ret = 0;
-        int val = 1;
-
-        String str = toString();
-
-        for ( int i = 0; i < Math.min( size, str.length()); i++ ) {
-            if ( str.charAt( i) == '1') {
-                ret += val;
-            }
-
-            val *= 2;
-        }
-
-        return ret;
-
-    }
-
-    public int toInt( int from, int size ) {
-        int ret = 0;
-        int val = 1;
-
-        String str = toString();
-
-        for ( int i = from; i < Math.min( from+size, str.length()); i++ ) {
-            if ( str.charAt( i) == '1') {
-                ret += val;
-            }
-
-            val *= 2;
-        }
-
-        return ret;
-    }
-
-    public DynamicBitArray getSlice( int from, int size ) {
-        DynamicBitArray ret = new DynamicBitArray();
-
-        for ( int i = from; i < from + size; ++i ) {
-            ret.append( bitAt(i));
-        }
-
-        return ret;
-    }
-
-    public boolean bitAt( int idx ) {
-        return (bytes.get(idx / 8).byteValue() & ((byte) (1 << (idx % 8)))) != 0; 
-    }
+    boolean bitAt( int idx );
 }
---
+++ modules/fausterize/src/org/openCage/huffman/DynmicBitArrayByBitSet.java
@@ -0,0 +1,81 @@
+package org.openCage.huffman;
+
+import java.util.BitSet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stephan
+ * Date: Mar 12, 2010
+ * Time: 3:24:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DynmicBitArrayByBitSet implements DynamicBitArray {
+
+    private BitSet bits   = new BitSet();
+    private int    length = 0;
+
+    @Override
+    public DynamicBitArray append(boolean bit) {
+        bits.set( length, bit );
+        length++;
+        return this;
+    }
+
+    @Override
+    public byte[] toByteArray() {
+        return new byte[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String toString() {
+        return bits.toString();
+    }
+
+    @Override
+    public String toString8() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public DynamicBitArray append(DynamicBitArray other) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public DynamicBitArray clone() {
+        DynmicBitArrayByBitSet next = new DynmicBitArrayByBitSet();
+        next.length = this.length;
+        next.bits = (BitSet)bits.clone();
+        return next;
+    }
+
+    @Override
+    public Byte getByteModulo(int idx) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int getBitSize() {
+        return length;
+    }
+
+    @Override
+    public int toInt(int size) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int toInt(int from, int size) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public DynamicBitArray getSlice(int from, int size) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean bitAt(int idx) {
+        return bits.get(idx);
+    }
+}
