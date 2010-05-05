482da344246e7aeffb544ecdaba8d6870eafd99b
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-05-05 14:18:36 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@542 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/lang/src/test/java/org/openCage/lang/structure/BFStackTest.java
+++ modules/lang/src/test/java/org/openCage/lang/structure/BFStackTest.java
@@ -3,20 +3,46 @@ package org.openCage.lang.structure;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-/**
- * Created by IntelliJ IDEA.
- * User: stephan
- * Date: May 2, 2010
- * Time: 1:33:26 PM
- * To change this template use File | Settings | File Templates.
- */
 public class BFStackTest {
 
+
+    @Test
+    public void testIsEmpty() {
+        BFStack<String> stack = new BFStack<String>();
+
+        assertTrue( stack.isEmpty() );
+
+        stack.push( "foo");
+        assertFalse( stack.isEmpty() );
+    }
+
     @Test( expected = IllegalStateException.class )
-    public void testInitial() {
+    public void testEmptyGetCurrent() {
         new BFStack<String>().getCurrent();
+    }
+
+    @Test
+    public void testHasBackWardBackward() {
+
+        BFStack<String> stack = new BFStack<String>();
+        stack.push( "foo");
+        stack.push( "dug");
+
+        assertTrue( stack.hasBackward());
+        stack.backward(); // no execption
+    }
+
+    @Test( expected = IllegalStateException.class )
+    public void testNotHasBackWardBackward() {
+
+        BFStack<String> stack = new BFStack<String>();
+        stack.push( "foo");
+
+        assertFalse( stack.hasBackward());
+        stack.backward(); // throws
     }
 
     @Test
@@ -36,6 +62,33 @@ public class BFStackTest {
 
         assertEquals( "foo", bf.backward() );
         assertEquals( "foo", bf.getCurrent() );
+
+    }
+
+    @Test
+    public void testBackwardForwardDeep() {
+        BFStack<String>  bf = new BFStack<String>();
+
+        bf.push( "A");
+        bf.push( "B");
+        bf.push( "C");
+        bf.push( "D");
+        bf.push( "E");
+
+        assertTrue( bf.hasBackward());
+        assertEquals( "D", bf.backward() );
+        assertEquals( "C", bf.backward());
+        assertEquals( "B", bf.backward());
+        assertEquals( "A", bf.backward());
+        assertFalse( bf.hasBackward());
+
+
+        assertTrue( bf.hasForward());
+        assertEquals( "B", bf.forward());
+        assertEquals( "C", bf.forward());
+        assertEquals( "D", bf.forward());
+        assertEquals( "E", bf.forward());
+        assertFalse( bf.hasForward());
 
     }
 
