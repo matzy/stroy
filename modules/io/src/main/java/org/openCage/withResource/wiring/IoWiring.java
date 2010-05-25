8ac00ecbd99ab79a0c8a29318f8c395d0bf7ef8d
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-05-25 05:41:07 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@589 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/io/src/main/java/org/openCage/withResource/wiring/IoWiring.java
+++ modules/io/src/main/java/org/openCage/withResource/wiring/IoWiring.java
@@ -1,31 +1,30 @@
-package org.openCage.withResource.wiring;
-
-import org.openCage.withResource.impl.WithImpl;
-import org.openCage.withResource.protocol.With;
-
-import com.google.inject.Binder;
-import com.google.inject.Module;
-
-public class IoWiring implements Module {
-    private static boolean once;
-
-    public void configure(Binder binder ) {
-//        if ( once ) {
-//            return;
-//        }
-//        once = true;
-
-        
-		binder.bind( With.class ).to( WithImpl.class );
-	}
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj != null && (obj instanceof IoWiring);
-    }
-}
+//package org.openCage.withResource.wiring;
+//
+//import org.openCage.withResource.impl.WithImpl;
+//
+//import com.google.inject.Binder;
+//import com.google.inject.Module;
+//
+//public class IoWiring implements Module {
+//    private static boolean once;
+//
+//    public void configure(Binder binder ) {
+////        if ( once ) {
+////            return;
+////        }
+////        once = true;
+//
+//
+//		binder.bind( With.class ).to( WithImpl.class );
+//	}
+//
+//    @Override
+//    public int hashCode() {
+//        return getClass().hashCode();
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        return obj != null && (obj instanceof IoWiring);
+//    }
+//}
