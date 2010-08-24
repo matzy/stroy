0702e8f8140d482c44de2008c01f7eb701f3e2fe
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-08-24 15:30:08 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@727 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ modules/artig-lib/src/test/java/org/openCage/artig/GetDeployedTest.java
@@ -0,0 +1,22 @@
+package org.openCage.artig;
+
+import org.junit.Test;
+import org.openCage.artig.stjx.Deployed;
+
+import java.io.InputStream;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+public class GetDeployedTest {
+
+    @Test
+    public void std() {
+
+        Deployed dpl = new GetDeployed( GetDeployed.class ).get();
+
+        assertNotNull( dpl );
+
+        assertEquals( "artig-lib", dpl.getArtifact().getName());
+    }
+}
