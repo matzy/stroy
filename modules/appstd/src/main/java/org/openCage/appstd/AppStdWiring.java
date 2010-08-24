4dafeec3240c29eabb8080a28b9537a569d0f98e
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-08-24 15:25:35 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@724 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/appstd/src/main/java/org/openCage/appstd/AppStdWiring.java
+++ modules/appstd/src/main/java/org/openCage/appstd/AppStdWiring.java
@@ -3,6 +3,7 @@ package org.openCage.appstd;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Singleton;
+import org.openCage.artig.stjx.Deployed;
 import org.openCage.io.SingletonApp;
 import org.openCage.lang.BackgroundExecutor;
 import org.openCage.lang.BackgroundExecutorImpl;
@@ -15,6 +16,15 @@ public class AppStdWiring implements Module {
 
         binder.bind(BackgroundExecutor.class ).to(BackgroundExecutorImpl.class );
 
+        binder.bind( Deployed.class ).toProvider( DeployedProvider.class );
 
+    }
+
+    @Override public boolean equals(Object o) {
+        return o instanceof AppStdWiring;
+    }
+
+    @Override public int hashCode() {
+        return AppStdWiring.class.hashCode();
     }
 }
--- modules/appstd/src/main/java/org/openCage/appstd/StdSingleton.java
+++ modules/appstd/src/main/java/org/openCage/appstd/StdSingleton.java
@@ -2,13 +2,14 @@ package org.openCage.appstd;
 
 import com.google.inject.Inject;
 import org.openCage.artig.stjx.Artifact;
+import org.openCage.artig.stjx.Deployed;
 import org.openCage.io.FriendlySingletonApp;
 import org.openCage.io.fspath.FSPathBuilder;
 
 public class StdSingleton extends FriendlySingletonApp {
 
     @Inject
-    public StdSingleton( Artifact arti ) {
-        super( FSPathBuilder.getPreferences().add( arti.getName(), "application.runs").toFile());
+    public StdSingleton( Deployed arti ) {
+        super( FSPathBuilder.getPreferences().add( arti.getArtifact().getName(), "application.runs").toFile());
     }
 }
