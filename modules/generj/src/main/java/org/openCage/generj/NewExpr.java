8a0f6984e0628ca8583af237f7c7af252dfae493
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-08-30 16:37:04 +0000

    more attributes
    git-svn-id: https://stroy.googlecode.com/svn/trunk@762 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/generj/src/main/java/org/openCage/generj/NewExpr.java
+++ modules/generj/src/main/java/org/openCage/generj/NewExpr.java
@@ -3,14 +3,19 @@ package org.openCage.generj;
 
 import org.openCage.lang.Strings;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class NewExpr implements Expr {
     private Typ typ;
-    private List<String> args;
+    private List<Expr> args = new ArrayList<Expr>();
 
-    public NewExpr( Typ typ, String ... args ) {
+    public static NewExpr NEW( Typ typ, Expr ... args ) {
+        return new NewExpr( typ, args );
+    }
+
+    public NewExpr( Typ typ, Expr ... args ) {
         this.typ = typ;
         if ( args.length > 0 ) {
             this.args.addAll( Arrays.asList( args ));
@@ -18,6 +23,10 @@ public class NewExpr implements Expr {
     }
 
     public String toString() {
+        if ( args.isEmpty() ) {
+            return "new " + typ + "()";    
+        }
+
         return "new " + typ + "( " + Strings.join( args ) + ")";
     }
 
