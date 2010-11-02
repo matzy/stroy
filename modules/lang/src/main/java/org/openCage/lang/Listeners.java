765fe64e70040270318895bf665d04ab34819d58
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-11-02 15:57:48 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@850 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ modules/lang/src/main/java/org/openCage/lang/Listeners.java
@@ -0,0 +1,21 @@
+package org.openCage.lang;
+
+import org.openCage.lang.functions.F1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listeners<T> {
+
+    private List<F1<Void,T>> listeners = new ArrayList<F1<Void,T>>();
+
+    public void shout( T news ) {
+        for ( F1<Void,T> listener : listeners  ) {
+            listener.call( news );
+        }
+    }
+
+    public void add( F1<Void,T> listener ) {
+        listeners.add( listener );
+    }
+}
