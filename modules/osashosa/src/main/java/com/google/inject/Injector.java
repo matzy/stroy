a7355769729b0584f0d91d24d56e910fd87a9526
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2010-11-05 14:21:58 +0000

    osashosa externalized
    git-svn-id: https://stroy.googlecode.com/svn/trunk@853 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- modules/osashosa/src/main/java/com/google/inject/Injector.java
+++
@@ -1,6 +0,0 @@
-package com.google.inject;
-
-public interface Injector {
-
-    <T> T getInstance( Class<T> tClass);
-}
--- modules/osashosa/src/main/java/com/google/inject/name/Named.java
+++
@@ -1,9 +0,0 @@
-package com.google.inject.name;
-
-@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@java.lang.annotation.Target({java.lang.annotation.ElementType.FIELD, java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.METHOD})
-@com.google.inject.BindingAnnotation
-public @interface Named {
-
-    java.lang.String value();
-}
