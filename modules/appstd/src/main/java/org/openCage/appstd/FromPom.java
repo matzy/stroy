0a34b18ebc09dbc6d4d7d91b3931db5f1cc7ed5b
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2011-04-04 09:22:58 +0000

    update
    git-svn-id: https://stroy.googlecode.com/svn/trunk@869 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ modules/appstd/src/main/java/org/openCage/appstd/FromPom.java
@@ -0,0 +1,27 @@
+package org.openCage.appstd;
+
+import java.io.InputStream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: SPF
+ * Date: 13.12.10
+ * Time: 11:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class FromPom {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public FromPom() {
+    }
+
+    public void tt() {
+        InputStream is = getClass().getResourceAsStream(".");
+
+
+    }
+}
