c09a707732be97c5f3cb2d7be1ff0a623470f829
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2011-04-04 09:22:37 +0000

    update
    git-svn-id: https://stroy.googlecode.com/svn/trunk@868 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ modules/fausterize/src/main/java/org/openCage/gpad/MvnMain.java
@@ -0,0 +1,16 @@
+package org.openCage.gpad;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: SPF
+ * Date: 13.12.10
+ * Time: 11:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class MvnMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello worl");
+    }
+
+}
