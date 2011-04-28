f6df6f400a6fef7de0771c92d7b59c115412c519
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2011-04-28 08:38:52 +0000

    variable bitsize
    git-svn-id: https://stroy.googlecode.com/svn/trunk@1005 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ projects/huffman/modules/huffman/src/test/java/org/openCage/huffman/HuffmanNTest.java
@@ -0,0 +1,29 @@
+package org.openCage.huffman;
+
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: SPF
+ * Date: 08.04.11
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class HuffmanNTest {
+
+    @Test
+    public void testA() {
+        String src = "abcabea";
+        DynamicBitArray dba = DynamicBitArrayDirect.valueOf( src.getBytes(Charset.forName("utf8")));
+
+        System.out.println( dba.toString8() );
+
+        HuffmanN hn = new HuffmanN( dba );
+
+        HuffmanN.printCodes( hn.getCode( 8 ));
+
+        HuffmanN.printCodes( new Canonical().canonisize( hn.getCode( 8 )));
+    }
+}
