05e7cfd7acdaf52da870147c16c605b8aca3cd9a
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2011-05-17 13:17:23 +0000

    comment unused class
    git-svn-id: https://stroy.googlecode.com/svn/trunk@1033 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- projects/huffman/modules/huffman/src/main/java/org/openCage/huffman/DynmicBitArrayByBitSet.java
+++ projects/huffman/modules/huffman/src/main/java/org/openCage/huffman/DynmicBitArrayByBitSet.java
@@ -1,97 +1,97 @@
-package org.openCage.huffman;
-
-import java.util.BitSet;
-
-/**
- * Created by IntelliJ IDEA.
- * User: stephan
- * Date: Mar 12, 2010
- * Time: 3:24:25 PM
- * To change this template use File | Settings | File Templates.
- */
-public class DynmicBitArrayByBitSet implements BitField {
-
-    private BitSet bits   = new BitSet();
-    private int    length = 0;
-
-    @Override
-    public BitField append(boolean bit) {
-        bits.set( length, bit );
-        length++;
-        return this;
-    }
-
-    @Override
-    public byte[] toByteArray() {
-        return new byte[0];  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public String toString() {
-        return bits.toString();
-    }
-
-    @Override
-    public String toString8() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public BitField append(BitField other) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public BitField clonePlusOne() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public BitField clone() {
-        DynmicBitArrayByBitSet next = new DynmicBitArrayByBitSet();
-        next.length = this.length;
-        next.bits = (BitSet)bits.clone();
-        return next;
-    }
-
-    @Override
-    public Byte getByteModulo(int idx) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public int size() {
-        return length;
-    }
-
-    @Override
-    public int getInt(int size) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public int getInt(int from, int size) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public BitField getSlice(int from, int size) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public BitField trimTo(int len) {
-        throw new Error( "not impl" );
-    }
-
-
-    @Override
-    public boolean get(int idx) {
-        return bits.get(idx);
-    }
-
-    @Override
-    public int compareTo(BitField dynamicBitArray) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-}
+//package org.openCage.huffman;
+//
+//import java.util.BitSet;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: stephan
+// * Date: Mar 12, 2010
+// * Time: 3:24:25 PM
+// * To change this template use File | Settings | File Templates.
+// */
+//public class DynmicBitArrayByBitSet implements BitField {
+//
+//    private BitSet bits   = new BitSet();
+//    private int    length = 0;
+//
+//    @Override
+//    public BitField append(boolean bit) {
+//        bits.set( length, bit );
+//        length++;
+//        return this;
+//    }
+//
+//    @Override
+//    public byte[] toByteArray() {
+//        return new byte[0];  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return bits.toString();
+//    }
+//
+//    @Override
+//    public String toString8() {
+//        return null;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public BitField append(BitField other) {
+//        return null;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public BitField clonePlusOne() {
+//        return null;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public BitField clone() {
+//        DynmicBitArrayByBitSet next = new DynmicBitArrayByBitSet();
+//        next.length = this.length;
+//        next.bits = (BitSet)bits.clone();
+//        return next;
+//    }
+//
+//    @Override
+//    public Byte getByteModulo(int idx) {
+//        return null;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public int size() {
+//        return length;
+//    }
+//
+//    @Override
+//    public int getInt(int size) {
+//        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public int getInt(int from, int size) {
+//        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public BitField getSlice(int from, int size) {
+//        return null;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//
+//    @Override
+//    public BitField trimTo(int len) {
+//        throw new Error( "not impl" );
+//    }
+//
+//
+//    @Override
+//    public boolean get(int idx) {
+//        return bits.get(idx);
+//    }
+//
+//    @Override
+//    public int compareTo(BitField dynamicBitArray) {
+//        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+//}
