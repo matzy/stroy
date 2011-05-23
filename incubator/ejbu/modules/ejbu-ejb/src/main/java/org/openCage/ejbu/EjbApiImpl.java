52ff856bfd22fe8e110a360bdcae9389f2ad1855
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2011-05-23 15:21:58 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@1048 37efa0d8-1a4f-0410-87b2-b979a28c5b93

---
+++ incubator/ejbu/modules/ejbu-ejb/src/main/java/org/openCage/ejbu/EjbApiImpl.java
@@ -0,0 +1,21 @@
+package org.openCage.ejbu;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stephan
+ * Date: 5/23/11
+ * Time: 10:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@javax.ejb.Stateless( mappedName = "MyEjb" )
+public class EjbApiImpl implements EjbApi
+{
+   public String berechne( String d1, String d2 )
+   {
+      try {
+         return "" + (Double.parseDouble( d1 ) + Double.parseDouble( d2 ));
+      } catch( Exception ex ) {
+         return "";
+      }
+   }
+}
