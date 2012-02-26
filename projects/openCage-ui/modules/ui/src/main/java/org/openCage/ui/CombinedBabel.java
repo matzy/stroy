9a861f9689578291c25070f6e00389351737675e
openCage <openCage@37efa0d8-1a4f-0410-87b2-b979a28c5b93>
2012-02-26 11:07:11 +0000

    git-svn-id: https://stroy.googlecode.com/svn/trunk@1080 37efa0d8-1a4f-0410-87b2-b979a28c5b93

--- projects/openCage-ui/modules/ui/src/main/java/org/openCage/ui/CombinedBabel.java
+++ projects/openCage-ui/modules/ui/src/main/java/org/openCage/ui/CombinedBabel.java
@@ -1,21 +1,21 @@
-package org.openCage.ui;
-
-import org.openCage.babel.LocalePreference;
-import org.openCage.babel.MenuText;
-import org.openCage.babel.StdUIText;
-import org.openCage.ui.wiring.UITexts;
-
-public class CombinedBabel {
-
-    public final MenuText menu;
-    public final StdUIText ui;
-    public final UITexts otherUI;
-
-    public CombinedBabel() {
-        LocalePreference lp = new LocalePreference();
-        menu = new MenuText( lp );
-        ui = new StdUIText( lp );
-        otherUI = new UITexts( lp );
-    }
-
-}
+//package org.openCage.ui;
+//
+//import org.openCage.babel.LocalePreference;
+//import org.openCage.babel.MenuText;
+//import org.openCage.babel.StdUIText;
+//import org.openCage.ui.wiring.UITexts;
+//
+//public class CombinedBabel {
+//
+//    public final MenuText menu;
+//    public final StdUIText ui;
+//    public final UITexts otherUI;
+//
+//    public CombinedBabel() {
+//        LocalePreference lp = new LocalePreference();
+//        menu = new MenuText( lp );
+//        ui = new StdUIText( lp );
+//        otherUI = new UITexts( lp );
+//    }
+//
+//}
